7d08f3f4c5087c6fe89065b32632dd84078ae9c0
Mitja Felicijan <mitja.felicijan@gmail.com>
2025-03-27 22:54:18 +0100

    Added loop example to default template

--- app/routes/default.lua
+++ app/routes/default.lua
@@ -6,6 +6,18 @@ function handler:get()
 	octx.template.render("default.html", {
 		title = "Hello, Oberon!",
 		message = "This is how you render templates.",
+		moons = {
+			"Moon (Earth's moon)",
+			"Titan (Saturn)",
+			"Europa (Jupiter)",
+			"Ganymede (Jupiter)",
+			"Io (Jupiter)",
+			"Enceladus (Saturn)",
+			"Callisto (Jupiter)",
+			"Triton (Neptune)",
+			"Phobos (Mars)",
+			"Charon (Pluto)",
+		},
 		environment = octx.environment,
 	})
 
